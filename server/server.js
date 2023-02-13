70617846b765b6a2ff20077f5e98bca2b882cfe0
cameron-louw <cameronlouw89@icloud.com>
2023-02-13 21:26:29 +1300

    test

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ server.use(express.static(path.join(__dirname, './public')))
 server.use(cors('*'))
 
 // DEPARTMENT OF CONSERVATION APIS
-server.get('/api/v1/tracks', (req, res) => {
+server.get('https://campanion.vercel.app/api/v1/tracks', (req, res) => {
   request
     .get(`https://api.doc.govt.nz/v1/tracks?coordinates=nzgd2000`)
     .set('x-api-key', process.env.DOC_API_KEY)
@@ -26,7 +26,7 @@ server.get('/api/v1/tracks', (req, res) => {
     .catch((e) => console.error(e))
 })
 
-server.get('/api/v1/campsites', (req, res) => {
+server.get('https://campanion.vercel.app/api/v1/campsites', (req, res) => {
   request
     .get(`https://api.doc.govt.nz/v2/campsites?coordinates=nzgd2000`)
     .set('x-api-key', process.env.DOC_API_KEY)
@@ -36,7 +36,7 @@ server.get('/api/v1/campsites', (req, res) => {
     .catch((e) => console.log(e.message))
 })
 
-server.get('/api/v1/campsite-news', (req, res) => {
+server.get('https://campanion.vercel.app/api/v1/campsite-news', (req, res) => {
   request
     .get(`https://api.doc.govt.nz/v2/campsites/alerts`)
     .set('x-api-key', process.env.DOC_API_KEY)
@@ -45,21 +45,7 @@ server.get('/api/v1/campsite-news', (req, res) => {
     })
     .catch((e) => console.log(e.message))
 })
-// GET /api/v1/unsplash
-// server.get('/api/v1/unsplash/:search', (req, res) => {
-//   request
-//     .get(
-//       `https://api.unsplash.com/search/photos?client_id=${accessKey}&page=1&query=${req.params.search}`
-//     )
-//     // .set('x-api-key', process.env.ACCESS_KEY)
-//     .then((result) => {
-//       res.json(result.body.results)
-//     })
-//     .catch((err) => {
-//       res.status(500).send(err.message)
-//     })
-// })
 
-server.use('https://campanion.vercel.app/api/v1/unsplash', unsplashRoutes)
+server.use('/api/v1/unsplash', unsplashRoutes)
 
 module.exports = server
