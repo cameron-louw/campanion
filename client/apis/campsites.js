ef6bdacfc5a8e66401db80941a8ff478d88cb8b4
cameron-louw <cameronlouw89@icloud.com>
2023-02-13 19:51:45 +1300

    test

--- client/apis/campsites.js
+++ client/apis/campsites.js
@@ -1,13 +1,19 @@
 import request from 'superagent'
 
 export function getCampsites() {
-  return request.get('/api/v1/campsites').then((res) => res.body)
+  return request
+    .get('campanion.vercel.app/api/v1/campsites')
+    .then((res) => res.body)
 }
 
 export function getCampsiteNews() {
-  return request.get('/api/v1/campsite-news').then((res) => res.body)
+  return request
+    .get('campanion.vercel.app/api/v1/campsite-news')
+    .then((res) => res.body)
 }
 
 export function getCampsiteDetailed() {
-  return request.get('/api/v1/campsite-details').then((res) => res.body)
+  return request
+    .get('campanion.vercel.app/api/v1/campsite-details')
+    .then((res) => res.body)
 }
