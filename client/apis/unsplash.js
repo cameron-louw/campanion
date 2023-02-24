f599a6b6822b53917821f74c7a2ec1defd13cd76
cameron-louw <cameronlouw89@icloud.com>
2023-02-24 15:42:32 +1300

    update

--- client/apis/unsplash.js
+++ client/apis/unsplash.js
@@ -3,7 +3,5 @@ import request from 'superagent'
 const endpoint = 'https://api.vercel.com'
 
 export function fetchImages(search) {
-  return request
-    .get(`/api/v1/unsplash/${search}`)
-    .then((res) => res.body)
+  return request.get(`/api/v1/unsplash/${search}`).then((res) => res.body)
 }
